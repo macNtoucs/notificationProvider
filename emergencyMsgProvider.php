3d27d13eb035e8f0fb2e24ed8053a32f4c6e62a8
Rick <hackerick4@gmail.com>
2014-05-07 18:01:23 +0800

    完成全校廣播

--- emergencyMsgProvider.php
+++ emergencyMsgProvider.php
@@ -1,11 +1,30 @@
 <?php
 	 include 'DB_service.php';
-
+     include_once "severAPI.php";
      /***prepare the request parameter***/
      //var $studentIDsArray = json_decode($POST["studentIDs"]);
-      $notificationContent = json_decode($_POST['msg']);
+      $notificationContent = $_POST['msg'];
 	// echo $notificationContent;
 	 $dbHelper = new DB_service;
-	 $res = $dbHelper->getAllDeviceTokens();
-     print_r($res);
-?>
+	 $deviceToken = $dbHelper->getAllDeviceTokens();
+     print_r($deviceToken);
+      
+
+     /*start to push*/
+   $alert = $notificationContent; 
+   $sound = "default";
+   $badge = 1;
+   $moduleName = "emergencyinfo";
+   $content = $notificationContent;
+  
+   $push = new Push($deviceToken);
+   $body['aps'] = array(
+            'alert' => $alert.'  Server Time:'.date('Y-m-d H:i:s'),
+            'sound' => $sound,
+            'badge' => (int)$badge
+        );
+   $body['moduleName'] = $moduleName;
+   $body['content'] = $content.'  Server Time:'.date('Y-m-d H:i:s');
+   $push->pushData($body); 
+
+ ?>
