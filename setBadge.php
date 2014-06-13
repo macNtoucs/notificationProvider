abdd75109234fdab57a5f56f6138bf9bf993fa2c
root <root@trafficinfo.(none)>
2014-06-13 05:13:46 -0400

    getBadge use GET method

--- setBadge.php
+++ setBadge.php
@@ -3,7 +3,7 @@
      include_once "severAPI.php";
      /***prepare the request parameter***/
      //var $studentIDsArray = json_decode($POST["studentIDs"]);
-      $token = $_GET['token_SET'];
+      $token = $_GET['token_SET']; 
 	$newBadgeVal = $_GET['newBadgeVal'];
  
 	 $dbHelper = new DB_service;
